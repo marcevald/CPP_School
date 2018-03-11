84172a209b2ac06eb68eb693bce272c77bae549c
marcevald <marc.evald@gmail.com>
2018-03-11 15:21:38 +0100

    CPR new function

---
+++ CPR/CPR/CprNr.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+
+class CprNr
+{
+public:
+	// Default constructor and destructor
+	CprNr();
+	~CprNr();
+	
+	
+	CprNr(string);
+
+
+
+	
+
+private:
+	string nummeret;
+};
